9065c8ca4ac99f3100dfcb028ad12d472913f243
Susan Traynor <traynosm@gmail.com>
2023-10-25 15:25:25 +0100

    Identity/DB set up

--- src/bad-each-way-finder-api/bad-each-way-finder-api/Program.cs
+++ src/bad-each-way-finder-api/bad-each-way-finder-api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using bad_each_way_finder_api.Areas.Identity.Data;
 namespace bad_each_way_finder_api
 {
     public class Program
@@ -5,6 +8,11 @@ namespace bad_each_way_finder_api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("IdentityDataContextConnection") ?? throw new InvalidOperationException("Connection string 'IdentityDataContextConnection' not found.");
+
+            builder.Services.AddDbContext<BadEachWayFinderContext>(options => options.UseSqlite(connectionString));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<BadEachWayFinderContext>();
 
             // Add services to the container.
 
