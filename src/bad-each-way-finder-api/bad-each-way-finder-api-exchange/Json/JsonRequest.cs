5064d9e0a35a8659edaf37f2d6de06ae4000f59b
Susan Traynor <traynosm@gmail.com>
2023-11-07 20:24:17 +0000

    initial classes for exchange integration

---
+++ src/bad-each-way-finder-api/bad-each-way-finder-api-exchange/Json/JsonRequest.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace bad_each_way_finder_api_exchange.Json
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class JsonRequest
+    {
+        public JsonRequest()
+        {
+            JsonRpc = "2.0";
+        }
+
+        [JsonProperty(PropertyName = "jsonrpc", NullValueHandling = NullValueHandling.Ignore)]
+        public string JsonRpc { get; set; }
+
+        [JsonProperty(PropertyName = "method")]
+        public string Method { get; set; }
+
+        [JsonProperty(PropertyName = "params")]
+        public object Params { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public object Id { get; set; }
+    }
+
+}
---
+++ src/bad-each-way-finder-api/bad-each-way-finder-api-exchange/Json/JsonResponse.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace bad_each_way_finder_api_exchange.Json
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class JsonResponse<T>
+    {
+        [JsonProperty(PropertyName = "jsonrpc", NullValueHandling = NullValueHandling.Ignore)]
+        public string JsonRpc { get; set; }
+
+        [JsonProperty(PropertyName = "result", NullValueHandling = NullValueHandling.Ignore)]
+        public T Result { get; set; }
+
+        [JsonProperty(PropertyName = "error", NullValueHandling = NullValueHandling.Ignore)]
+        public System.Exception Error { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public object Id { get; set; }
+
+        [JsonIgnore]
+        public bool HasError
+        {
+            get { return Error != null; }
+        }
+    }
+}
