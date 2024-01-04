afa717cc3d3d116cfd363df5ad485f7d1d5f36ed
Susan Traynor <traynosm@gmail.com>
2024-01-04 20:11:33 +0000

    unsaved from previous commit

--- src/bad-each-way-finder-api/bad-each-way-finder-api/Controllers/AccountController.cs
+++ src/bad-each-way-finder-api/bad-each-way-finder-api/Controllers/AccountController.cs
@@ -1,12 +1,10 @@
 ﻿using bad_each_way_finder_api_domain.CommonInterfaces;
-using bad_each_way_finder_api_domain.DomainModel;
 using bad_each_way_finder_api_domain.DTO;
 using Microsoft.AspNetCore.Mvc;
-using NuGet.Common;
 
 namespace bad_each_way_finder_api.Controllers
 {
-   
+
     [Route("api/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
